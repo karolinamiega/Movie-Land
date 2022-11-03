9735b731c7d159cd7d16319cbb92494c5dadf6bf
karolinamiega <nugaraitekarolina@gmail.com>
2022-11-03 21:37:41 +0200

    2022-11-03

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import SearchIcon from "./search.svg";
 
 const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=a5e43c36";
 
-const movie1 = {
-  Title: "Shrek",
-  Year: "2001",
-  imdbID: "tt0126029",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-};
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState([]);
